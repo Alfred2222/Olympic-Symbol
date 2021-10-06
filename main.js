48e15c1e13bc9175c6da5ad5f84237f78ced7975
Alfred2222 <84137430+Alfred2222@users.noreply.github.com>
2021-10-06 16:47:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,31 +2,37 @@ canvas=document.getElementById("olympic_canvas");
 ctx=canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.strokeStyle="blue";
-ctx.lineWidth=5;
-ctx.arc(200,200,40,0,4*Math.PI);
+ctx.strokeStyl="grey";
+ctx.lineWidth=1
+ctx.rect(200,120,430,200);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle="black";
+ctx.strokeStyle="blue";
 ctx.lineWidth=5;
 ctx.arc(300,200,40,0,4*Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.strokeStyle="red";
+ctx.strokeStyle="black";
 ctx.lineWidth=5;
 ctx.arc(400,200,40,0,4*Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=5;
+ctx.arc(500,200,40,0,4*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
 ctx.strokeStyle="yellow";
 ctx.lineWidth=5;
-ctx.arc(250,240,40,0,4*Math.PI);
+ctx.arc(350,240,40,0,4*Math.PI);
 ctx.stroke();
 
 ctx.beginPath();
 ctx.strokeStyle="green";
 ctx.lineWidth=5;
-ctx.arc(350,240,40,0,4*Math.PI);
+ctx.arc(450,240,40,0,4*Math.PI);
 ctx.stroke();
